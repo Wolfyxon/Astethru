2a36a8be0a264e1868838b2c25c0695b84f01384
Wolfyxon <wolfyxon@gmail.com>
2024-12-08 14:27:28 +0100

    Exitting

--- src/main.c
+++ src/main.c
@@ -5,7 +5,12 @@ int main(void) {
     SDL_Init(SDL_INIT_EVERYTHING);
 
     SDL_Window* window = SDL_CreateWindow("Astethru", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 500, 500, 0);
+    SDL_Event event;
 
-    while (1);
+    while (1) {    
+        SDL_PollEvent(&event);
+        if(event.type == SDL_QUIT) break;
+    }
+
     return 0;
 }
