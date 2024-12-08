4eb205e6e86b5dbd86d43155e5497687d8af8c41
Wolfyxon <wolfyxon@gmail.com>
2024-12-08 21:55:52 +0100

    Implemented SpriteArray_new()

--- src/spriteArray/spriteArray.c
+++ src/spriteArray/spriteArray.c
@@ -1,1 +1,10 @@
 #include "spriteArray.h"
+
+SpriteArray SpriteArray_new(int size) {
+    SpriteArray* arr = malloc(sizeof(arr) + sizeof(Sprite) * size);
+    
+    arr->length = 0;
+    arr->maxLength = size;
+
+    return *arr; 
+}
