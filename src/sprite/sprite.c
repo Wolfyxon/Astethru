763895db27206d745c67455141e8b0bd3bfb8029
Wolfyxon <wolfyxon@gmail.com>
2024-12-08 15:47:28 +0100

    Sprites also contain pointer to their renderer

--- src/sprite/sprite.c
+++ src/sprite/sprite.c
@@ -4,7 +4,8 @@ Sprite loadSprite(SDL_Renderer* renderer, const char* path) {
     Sprite s = {
         .x = 0,
         .y = 0,
-        .texture = IMG_LoadTexture(renderer, path)
+        .texture = IMG_LoadTexture(renderer, path),
+        .renderer = renderer
     };
 
     return s;
--- src/sprite/sprite.h
+++ src/sprite/sprite.h
@@ -5,6 +5,7 @@ typedef struct {
     float x;
     float y;
     SDL_Texture* texture;
+    SDL_Renderer* renderer;
 } Sprite;
 
 Sprite loadSprite(SDL_Renderer* renderer, const char* path);
