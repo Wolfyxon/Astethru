ff2ccd28d4da089b1c83db6a3a9e226021b7ce6f
Wolfyxon <wolfyxon@gmail.com>
2024-12-08 15:45:27 +0100

    Better window init

--- src/main.c
+++ src/main.c
@@ -6,7 +6,13 @@
 int main(void) {
     SDL_Init(SDL_INIT_EVERYTHING);
 
-    SDL_Window* window = SDL_CreateWindow("Astethru", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_SIZE, WINDOW_SIZE, 0);
+    SDL_Window* window = SDL_CreateWindow(
+        "Astethru", 
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 
+        WINDOW_SIZE, WINDOW_SIZE, 
+        0
+    );
+    
     SDL_Renderer* rend = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
     SDL_Event event;
