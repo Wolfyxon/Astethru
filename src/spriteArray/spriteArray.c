ad6aa175c7ffa7aeb188276b6321c72adcabf708
Wolfyxon <wolfyxon@gmail.com>
2024-12-08 22:18:09 +0100

    Error when allocation fails

--- src/spriteArray/spriteArray.c
+++ src/spriteArray/spriteArray.c
@@ -1,8 +1,14 @@
 #include "spriteArray.h"
+#include <stdio.h>
 
 SpriteArray* SpriteArray_new(int size) {
-    SpriteArray* arr = malloc(sizeof(SpriteArray) + sizeof(Sprite) * size);
+    int msize = sizeof(SpriteArray) + sizeof(Sprite) * size;
+    SpriteArray* arr = malloc(msize);
     
+    if(arr == NULL) {
+        fprintf(stderr, "Failed to allocate memory for SpriteArray. Size: %i Memory size: %i \n", size, msize);
+    }
+
     arr->length = 0;
     arr->maxLength = size;
 
